d8eea674edbca29a5873d1c4db6236b2f250575f
xiligey <635943647@qq.com>
2019-10-29 11:33:04 +0800

    feat: add tree models

---
+++ tree/DecisionTree.py
@@ -0,0 +1,10 @@
+from ..base import ClassifierMixin
+
+
+class DecisionTree(ClassifierMixin):
+    def __init__(self, ):
+        super(DecisionTree, self).__init__()
+
+
+class Node(object):
+    """"""
